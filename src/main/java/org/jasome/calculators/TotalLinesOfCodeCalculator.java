5e06dae5c6694f6de94247a6d590563b13f18d26
Rod Hilton <rod.hilton@gmail.com>
2017-01-26 23:29:40 -0700

    cleanup

--- src/main/java/org/jasome/calculators/TotalLinesOfCodeCalculator.java
+++ src/main/java/org/jasome/calculators/TotalLinesOfCodeCalculator.java
@@ -1,6 +1,5 @@
 package org.jasome.calculators;
 
-import com.github.javaparser.Position;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
 import com.github.javaparser.ast.body.*;
@@ -16,7 +15,9 @@ import org.jasome.Calculator;
 import org.jasome.SomeClass;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.Optional;
+import java.util.Set;
+import java.util.Stack;
 
 public class TotalLinesOfCodeCalculator implements Calculator<SomeClass> {
 
@@ -31,139 +32,135 @@ public class TotalLinesOfCodeCalculator implements Calculator<SomeClass> {
 
         int count = 0;
 
-        while(!nodeStack.empty()) {
+        while (!nodeStack.empty()) {
             Node node = nodeStack.pop();
 
-            //System.out.println("Parsing lines "+node.getBegin().get().line + "-"+node.getEnd().get().line+", current count: "+count);
-
-            if(node instanceof FieldDeclaration) {
+            if (node instanceof FieldDeclaration) {
                 count = count + 1;
-            } else if(node instanceof MethodDeclaration ) {
+            } else if (node instanceof MethodDeclaration) {
                 count = count + 1; //for the opening of the method
                 Optional<BlockStmt> body = ((MethodDeclaration) node).getBody();
-                if(body.isPresent()) {
+                if (body.isPresent()) {
                     count = count + 1; //for the closing, only happens if there's a body
                     nodeStack.add(body.get());
                 }
-            } else if(node instanceof InitializerDeclaration) {
+            } else if (node instanceof InitializerDeclaration) {
                 count = count + 2; //for the opening and closing of the initializer block
                 nodeStack.add(((InitializerDeclaration) node).getBody());
-            } else if(node instanceof ConstructorDeclaration) {
+            } else if (node instanceof ConstructorDeclaration) {
                 count = count + 2; //for the opening and closing of the method
-                nodeStack.add(((ConstructorDeclaration)node).getBody());
-            } else if(node instanceof ClassOrInterfaceDeclaration) {
+                nodeStack.add(((ConstructorDeclaration) node).getBody());
+            } else if (node instanceof ClassOrInterfaceDeclaration) {
                 count = count + 2; //for the opening and closing of the declaration
-                ClassOrInterfaceDeclaration classOrInterfaceDeclaration = (ClassOrInterfaceDeclaration)node;
+                ClassOrInterfaceDeclaration classOrInterfaceDeclaration = (ClassOrInterfaceDeclaration) node;
                 nodeStack.addAll(classOrInterfaceDeclaration.getMembers());
-            } else if(node instanceof EnumDeclaration) {
+            } else if (node instanceof EnumDeclaration) {
                 count = count + 2; //for the opening and closing of the declaration
-                EnumDeclaration enumDeclaration = (EnumDeclaration)node;
+                EnumDeclaration enumDeclaration = (EnumDeclaration) node;
                 nodeStack.addAll(enumDeclaration.getEntries());
                 nodeStack.addAll(enumDeclaration.getMembers());
-            } else if(node instanceof EnumConstantDeclaration) {
+            } else if (node instanceof EnumConstantDeclaration) {
                 count = count + 1;
-                EnumConstantDeclaration enumConstantDeclaration = (EnumConstantDeclaration)node;
-                if(enumConstantDeclaration.getClassBody().size() > 0) {
+                EnumConstantDeclaration enumConstantDeclaration = (EnumConstantDeclaration) node;
+                if (enumConstantDeclaration.getClassBody().size() > 0) {
                     count = count + 1; //for the closing, we already counted the opening
                     nodeStack.addAll(enumConstantDeclaration.getClassBody());
                 }
-            } else if(node instanceof AnnotationDeclaration) {
+            } else if (node instanceof AnnotationDeclaration) {
                 count = count + 2; //for the opening and closing of the declaration
-                AnnotationDeclaration annotationDeclaration = (AnnotationDeclaration)node;
+                AnnotationDeclaration annotationDeclaration = (AnnotationDeclaration) node;
                 nodeStack.addAll(annotationDeclaration.getMembers());
-            } else if(node instanceof EmptyMemberDeclaration) {
+            } else if (node instanceof EmptyMemberDeclaration) {
                 //Ignore, it's empty
-            } else if(node instanceof AnnotationMemberDeclaration) {
+            } else if (node instanceof AnnotationMemberDeclaration) {
                 count = count + 1;
-            } else if(node instanceof LocalClassDeclarationStmt) {
+            } else if (node instanceof LocalClassDeclarationStmt) {
                 //Count nothing, this contains a ClassOrInterfaceDeclaration which will count the 2 lines
-                nodeStack.add(((LocalClassDeclarationStmt)node).getClassDeclaration());
-            } else if(node instanceof ExplicitConstructorInvocationStmt) {
+                nodeStack.add(((LocalClassDeclarationStmt) node).getClassDeclaration());
+            } else if (node instanceof ExplicitConstructorInvocationStmt) {
                 count = count + 1;
-            } else if(node instanceof BlockStmt) {
+            } else if (node instanceof BlockStmt) {
                 NodeList<Statement> statements = ((BlockStmt) node).getStatements();
                 nodeStack.addAll(statements);
-            } else if(node instanceof ForStmt) {
+            } else if (node instanceof ForStmt) {
                 count = count + 2; //2 for the opening and closing of the for statement, we ignore complexity within the loop itself
                 nodeStack.add(((ForStmt) node).getBody());
-            } else if(node instanceof ForeachStmt) {
+            } else if (node instanceof ForeachStmt) {
                 count = count + 2; //2 for the opening and closing of the foreach statement
                 nodeStack.add(((ForeachStmt) node).getBody());
-            } else if(node instanceof WhileStmt) {
+            } else if (node instanceof WhileStmt) {
                 count = count + 2; //2 for the opening and closing of the while statement
-                nodeStack.add(((WhileStmt)node).getBody());
-            } else if(node instanceof DoStmt) {
+                nodeStack.add(((WhileStmt) node).getBody());
+            } else if (node instanceof DoStmt) {
                 count = count + 2; //2 for the opening and closing of the do statement
-                nodeStack.add(((DoStmt)node).getBody());
-            } else if(node instanceof ClassOrInterfaceType) {
+                nodeStack.add(((DoStmt) node).getBody());
+            } else if (node instanceof ClassOrInterfaceType) {
                 //Ignore
-            } else if(node instanceof SingleMemberAnnotationExpr || node instanceof MarkerAnnotationExpr || node instanceof NormalAnnotationExpr) {
+            } else if (node instanceof SingleMemberAnnotationExpr || node instanceof MarkerAnnotationExpr || node instanceof NormalAnnotationExpr) {
                 //Ignore, we don't count annotations
-            } else if(node instanceof TypeParameter) {
+            } else if (node instanceof TypeParameter) {
                 //Type parameters (generics) are not statements and don't count
-            } else if(node instanceof SynchronizedStmt) {
+            } else if (node instanceof SynchronizedStmt) {
                 count = count + 2; //this is a synchronized block, not a modifier, so it's got an opening and closing
-                nodeStack.add(((SynchronizedStmt)node).getBody());
-            } else if(node instanceof ExpressionStmt) {
+                nodeStack.add(((SynchronizedStmt) node).getBody());
+            } else if (node instanceof ExpressionStmt) {
                 count = count + 1;
                 //nodeStack.add(((ExpressionStmt)node).getExpression()); //TODO: maybe be a little smarter, go into more detail here since there can be so much code in an expression.  same for the statements below
-            } else if(node instanceof EmptyStmt) {
+            } else if (node instanceof EmptyStmt) {
                 //It's empty, ignore.  Usually this is just a double semicolon
-            } else if(node instanceof AssertStmt) {
+            } else if (node instanceof AssertStmt) {
                 count = count + 1;
-            } else if(node instanceof ReturnStmt) {
+            } else if (node instanceof ReturnStmt) {
                 count = count + 1;
-            } else if(node instanceof ThrowStmt) {
+            } else if (node instanceof ThrowStmt) {
                 count = count + 1;
-            } else if(node instanceof BreakStmt) {
+            } else if (node instanceof BreakStmt) {
                 count = count + 1;
-            } else if(node instanceof ContinueStmt) {
+            } else if (node instanceof ContinueStmt) {
                 count = count + 1;
-            } else if(node instanceof LabeledStmt) {
+            } else if (node instanceof LabeledStmt) {
                 count = count + 1;
-            } else if(node instanceof IfStmt) {
+            } else if (node instanceof IfStmt) {
                 count = count + 2;
                 IfStmt ifStmt = (IfStmt) node;
                 nodeStack.add(ifStmt.getThenStmt());
-                if(ifStmt.getElseStmt().isPresent()) {
+                if (ifStmt.getElseStmt().isPresent()) {
                     count = count + 2;
                     nodeStack.add(ifStmt.getElseStmt().get());
                 }
-            } else if(node instanceof TryStmt) {
+            } else if (node instanceof TryStmt) {
                 TryStmt tryStmt = (TryStmt) node;
-                if(tryStmt.getTryBlock().isPresent()) {
+                if (tryStmt.getTryBlock().isPresent()) {
                     count = count + 2;
                     nodeStack.add(tryStmt.getTryBlock().get());
                 }
-                for(CatchClause catchClause: tryStmt.getCatchClauses()) {
+                for (CatchClause catchClause : tryStmt.getCatchClauses()) {
                     count = count + 2;
                     nodeStack.add(catchClause.getBody());
                 }
 
-                if(tryStmt.getFinallyBlock().isPresent()) {
+                if (tryStmt.getFinallyBlock().isPresent()) {
                     count = count + 2;
                     nodeStack.add(tryStmt.getFinallyBlock().get());
                 }
-            } else if(node instanceof SwitchStmt) {
+            } else if (node instanceof SwitchStmt) {
                 count = count + 2;
                 SwitchStmt switchStmt = (SwitchStmt) node;
                 nodeStack.addAll(switchStmt.getEntries());
-            } else if(node instanceof SwitchEntryStmt) {
+            } else if (node instanceof SwitchEntryStmt) {
                 count = count + 1;
                 SwitchEntryStmt switchEntryStmt = (SwitchEntryStmt) node;
                 nodeStack.addAll(switchEntryStmt.getStatements());
             } else {
-                System.out.println("Encountered type I'm not ready for: "+node.getClass());
-                System.out.println("Lines "+node.getBegin().get().line+ " to "+node.getEnd().get().line+"\n");
+                System.err.println("Encountered type I'm not ready for: " + node.getClass());
+                System.err.println("Lines " + node.getBegin().get().line + " to " + node.getEnd().get().line + "\n");
             }
         }
 
         Calculation result = new Calculation(
                 "TLOC",
                 "Total Lines of Code Count",
-                new BigDecimal(
-                        count //+2 //for the opening and closing of the class declaration itself
-                )
+                new BigDecimal(count)
         );
 
         return Sets.newHashSet(result);
