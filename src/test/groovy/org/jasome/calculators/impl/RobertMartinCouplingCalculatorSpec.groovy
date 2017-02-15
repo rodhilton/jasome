43316c773ec613f5e1c0d2186b44ceb4ac8e5c96
Rod Hilton <rod.hilton@gmail.com>
2017-02-14 22:13:58 -0700

    more tests for robert martin metrics

--- src/test/groovy/org/jasome/calculators/impl/RobertMartinCouplingCalculatorSpec.groovy
+++ src/test/groovy/org/jasome/calculators/impl/RobertMartinCouplingCalculatorSpec.groovy
@@ -1,9 +1,9 @@
 package org.jasome.calculators.impl
 
-import org.jasome.parsing.Type
 import spock.lang.Specification
 
 import static org.jasome.util.Matchers.containsMetric
+import static org.jasome.util.Matchers.doesNotContainMetric
 import static org.jasome.util.TestUtil.projectFromSnippet
 import static spock.util.matcher.HamcrestSupport.expect
 
@@ -110,9 +110,163 @@ class RobertMartinCouplingCalculatorSpec extends Specification {
         expect firstResult, containsMetric("I", new BigDecimal(0.500000))
         expect firstResult, containsMetric("A", new BigDecimal(0.250000))
         expect firstResult, containsMetric("DMS", new BigDecimal(0.250000))
-        expect firstResult, containsMetric("NOI", 1)
+        expect firstResult, containsMetric("NOI", BigDecimal.ONE)
     }
 
-    //TODO: assumes overlapping names are within same package if there is a match
-    //TODO: test that notices that anything that's not public isn't counted in the case of overlapping names
+
+    def "calculate assumes contradictory names are used within the package"() {
+
+        given:
+        def project = projectFromSnippet '''
+        package org.whatever.stuff;
+        
+        import org.whatever.stuff2.*;
+        
+        public class A {
+            public void helloA() {
+                System.out.println("Hello A");
+            }
+        }
+        
+        ''','''
+        package org.whatever.stuff2;
+        
+        import org.whatever.stuff.*;
+
+        public class A {
+            public void otherHelloA() {
+                System.out.println("Hello A 2");
+            }
+        }
+        ''','''
+        package org.whatever.stuff2;
+
+        import org.whatever.stuff.*;
+        
+        public class B {
+            public static int utilMethod(A a) {
+                return 100;
+            }
+        }
+        '''
+
+        org.jasome.parsing.Package firstPackage = (project.getPackages() as List<Package>).find{p -> p.name=="org.whatever.stuff"}
+        org.jasome.parsing.Package secondPackage = (project.getPackages() as List<Package>).find{p -> p.name=="org.whatever.stuff2"}
+
+
+        when:
+        def firstResult = new RobertMartinCouplingCalculator().calculate(firstPackage);
+        def secondResult = new RobertMartinCouplingCalculator().calculate(secondPackage);
+
+        then:
+        expect firstResult, containsMetric("Ce", BigDecimal.ZERO)
+        expect firstResult, containsMetric("Ca", BigDecimal.ZERO)
+
+        expect secondResult, containsMetric("Ce", BigDecimal.ZERO)
+        expect secondResult, containsMetric("Ca", BigDecimal.ZERO)
+
+        expect firstResult, doesNotContainMetric("I")
+        expect secondResult, containsMetric("Ca", new BigDecimal("0.000000"))
+        expect firstResult, doesNotContainMetric("DMS")
+        expect firstResult, containsMetric("NOI", BigDecimal.ZERO)
+    }
+
+    def "doesn't count non-public classes"() {
+
+        given:
+        def project = projectFromSnippet '''
+        package org.whatever.stuff;
+        
+        import org.whatever.stuff2.*;
+        
+        public class A {
+            public void helloA() {
+                System.out.println("Hello A");
+            }
+        }
+        
+        class B {
+            public void helloB() {
+                System.out.println("Hello B");
+            }
+        }        
+        ''','''
+        package org.whatever.stuff2;
+        
+        import org.whatever.stuff.*;
+
+        public class C {
+            public void sayHello() {
+                new B().helloB();
+            }
+        }
+        '''
+
+        org.jasome.parsing.Package firstPackage = (project.getPackages() as List<Package>).find{p -> p.name=="org.whatever.stuff"}
+        org.jasome.parsing.Package secondPackage = (project.getPackages() as List<Package>).find{p -> p.name=="org.whatever.stuff2"}
+
+
+        when:
+        def firstResult = new RobertMartinCouplingCalculator().calculate(firstPackage);
+        def secondResult = new RobertMartinCouplingCalculator().calculate(secondPackage);
+
+        then:
+        expect firstResult, containsMetric("Ce", BigDecimal.ZERO)
+        expect firstResult, containsMetric("Ca", BigDecimal.ZERO)
+
+        expect secondResult, containsMetric("Ce", BigDecimal.ZERO)
+        expect secondResult, containsMetric("Ca", BigDecimal.ZERO)
+
+        expect firstResult, doesNotContainMetric("I")
+        expect secondResult, containsMetric("Ca", new BigDecimal("0.000000"))
+        expect firstResult, doesNotContainMetric("DMS")
+        expect firstResult, containsMetric("NOI", BigDecimal.ZERO)
+    }
+
+    def "able to see public static classes"() {
+
+        given:
+        def project = projectFromSnippet '''
+        package org.whatever.stuff;
+        
+        import org.whatever.stuff2.*;
+        
+        public class A {
+            public void helloA() {
+                System.out.println("Hello A");
+            }
+            
+            public static class Inner {
+                public static void sayHello() {
+                    System.out.println("hi");
+                }
+            }
+        }            
+        ''','''
+        package org.whatever.stuff2;
+        
+        import org.whatever.stuff.*;
+
+        public class B {
+            public void sayHello() {
+                A.Inner.sayHello();
+            }
+        }
+        '''
+
+        org.jasome.parsing.Package firstPackage = (project.getPackages() as List<Package>).find{p -> p.name=="org.whatever.stuff"}
+        org.jasome.parsing.Package secondPackage = (project.getPackages() as List<Package>).find{p -> p.name=="org.whatever.stuff2"}
+
+
+        when:
+        def firstResult = new RobertMartinCouplingCalculator().calculate(firstPackage);
+        def secondResult = new RobertMartinCouplingCalculator().calculate(secondPackage);
+
+        then:
+        expect firstResult, containsMetric("Ce", BigDecimal.ZERO)
+        expect firstResult, containsMetric("Ca", BigDecimal.ONE)
+
+        expect secondResult, containsMetric("Ce", BigDecimal.ONE)
+        expect secondResult, containsMetric("Ca", BigDecimal.ZERO)
+    }
 }
--- src/test/java/org/jasome/util/Matchers.java
+++ src/test/java/org/jasome/util/Matchers.java
@@ -31,4 +31,21 @@ public class Matchers {
     public static Matcher<Set<Metric>> containsMetric(String name, double value) {
         return containsMetric(name, new BigDecimal(value));
     }
+
+    public static Matcher<Set<Metric>> doesNotContainMetric(String name) {
+        return new BaseMatcher<Set<Metric>>() {
+            @Override
+            @SuppressWarnings("unchecked")
+            public boolean matches(final Object item) {
+                final Set<Metric> metrics = (Set<Metric>) item;
+                Optional<Metric> namedMetric = metrics.stream().filter((m) -> m.getName().equalsIgnoreCase(name)).findFirst();
+                return !namedMetric.isPresent();
+            }
+
+            @Override
+            public void describeTo(final Description description) {
+                description.appendText("expected metrics to not contain").appendValue(name);
+            }
+        };
+    }
 }
