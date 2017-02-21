3dd00c3130c2e8a96d8e6d3ba431a98914427c56
Rod Hilton <rod.hilton@gmail.com>
2017-02-20 17:06:42 -0700

    uncommenting out other processors

--- src/main/java/org/jasome/executive/ProcessorFactory.java
+++ src/main/java/org/jasome/executive/ProcessorFactory.java
@@ -6,28 +6,28 @@ import org.jasome.input.Processor;
 class ProcessorFactory {
     static Processor getProcessor() {
         Processor processor = new Processor();
-//
-//        processor.registerTypeCalculator(new RawTotalLinesOfCodeCalculator());
-//
-//        processor.registerTypeCalculator(new NumberOfFieldsCalculator());
-//
-//        processor.registerProjectCalculator(new TotalLinesOfCodeCalculator.ProjectCalculator());
-//        processor.registerPackageCalculator(new TotalLinesOfCodeCalculator.PackageCalculator());
-//        processor.registerTypeCalculator(new TotalLinesOfCodeCalculator.TypeCalculator());
-//        processor.registerMethodCalculator(new TotalLinesOfCodeCalculator.MethodCalculator());
-//
-//        processor.registerMethodCalculator(new CyclomaticComplexityCalculator());
-//        processor.registerTypeCalculator(new WeightedMethodsCalculator());
-//
-//        processor.registerMethodCalculator(new NumberOfParametersCalculator());
-//        processor.registerPackageCalculator(new NumberOfClassesCalculator());
-//
-//        processor.registerTypeCalculator(new SpecializationIndexCalculator());
-//
-//        processor.registerPackageCalculator(new RobertMartinCouplingCalculator());
-//
-//        processor.registerMethodCalculator(new NestedBlockDepthCalculator());
-//        processor.registerTypeCalculator(new LackOfCohesionMethodsCalculator());
+
+        processor.registerTypeCalculator(new RawTotalLinesOfCodeCalculator());
+
+        processor.registerTypeCalculator(new NumberOfFieldsCalculator());
+
+        processor.registerProjectCalculator(new TotalLinesOfCodeCalculator.ProjectCalculator());
+        processor.registerPackageCalculator(new TotalLinesOfCodeCalculator.PackageCalculator());
+        processor.registerTypeCalculator(new TotalLinesOfCodeCalculator.TypeCalculator());
+        processor.registerMethodCalculator(new TotalLinesOfCodeCalculator.MethodCalculator());
+
+        processor.registerMethodCalculator(new CyclomaticComplexityCalculator());
+        processor.registerTypeCalculator(new WeightedMethodsCalculator());
+
+        processor.registerMethodCalculator(new NumberOfParametersCalculator());
+        processor.registerPackageCalculator(new NumberOfClassesCalculator());
+
+        processor.registerTypeCalculator(new SpecializationIndexCalculator());
+
+        processor.registerPackageCalculator(new RobertMartinCouplingCalculator());
+
+        processor.registerMethodCalculator(new NestedBlockDepthCalculator());
+        processor.registerTypeCalculator(new LackOfCohesionMethodsCalculator());
 
         processor.registerTypeCalculator(new ClassInheritanceCalculator());
         return processor;
