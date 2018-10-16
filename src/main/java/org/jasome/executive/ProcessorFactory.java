82486d1386c3e1c756febeb8b983b63b56c77443
Rod Hilton <rod.hilton@gmail.com>
2018-10-16 09:22:13 -0600

    Re-enable 2 calculators (need to investigate issues)

--- src/main/java/org/jasome/executive/ProcessorFactory.java
+++ src/main/java/org/jasome/executive/ProcessorFactory.java
@@ -33,8 +33,8 @@ class ProcessorFactory {
 
         processor.registerTypeCalculator(new MethodAndAttributeInheritanceCalculator());
 
-        //processor.registerMethodCalculator(new FanCalculator());
-        //processor.registerTypeCalculator(new LinkCalculator());
+        processor.registerMethodCalculator(new FanCalculator());
+        processor.registerTypeCalculator(new LinkCalculator());
         processor.registerMethodCalculator(new McclureCalculator());
 
         processor.registerTypeCalculator(new TypeAggregatorCalculator());
